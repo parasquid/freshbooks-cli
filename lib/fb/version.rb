3d290995702a4c9af4a59b1d02dd319b875e67da
parasquid <git@parasquid.com>
2026-03-31 18:39:29 +0800

    refactor: rename FB::VERSION and FB::Spinner to FreshBooks::CLI

--- lib/fb/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module FB
-  VERSION = "0.3.3"
-end
---
+++ lib/freshbooks/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module FreshBooks
+  module CLI
+    VERSION = "0.3.3"
+  end
+end
