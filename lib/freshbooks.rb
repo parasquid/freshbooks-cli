fcd9b616f6920fc5536069f4cdaae4ad2012537d
parasquid <git@parasquid.com>
2026-03-31 19:57:28 +0800

    refactor: rename main require file and update entry points

---
+++ lib/freshbooks.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "freshbooks/version"
+require_relative "freshbooks/spinner"
+require_relative "freshbooks/auth"
+require_relative "freshbooks/api"
+require_relative "freshbooks/cli"
